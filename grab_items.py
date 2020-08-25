3a5637dd86289274ccbb456968034caa7036ca03
khangvu12 <khang.vu@f4.intek.edu.vn>
2020-08-25 23:48:34 +0700

    reformat output

--- grab_items.py
+++ grab_items.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 import argparse
 import feedparser
+import datetime
+
+def get_items(urls):
+    result = {}
+    # Add the timestamp
+    result["date"] = str(datetime.datetime.utcnow())
+    result["feeds"] = {}
+
+    for url in urls:
+        # Ignore empty urls
+        if not url:
+            continue
+        # Parse items from the url
+        feed = feedparser.parse(url)
+        items = feed["items"]
+
+        # Remove the key 'published_parsed' which doesn't follow the JSON format
+        for item in items:
+            item.pop("published_parsed", None)
+
+        # Store the result
+        result["feeds"] = {url : items}
+
+    return result
+
 
 def main():
     description = "Read the feed from urls"
@@ -13,19 +38,8 @@ def main():
     # List of urls
     urls = ''.join(args['urls']).split(',')
 
-    for url in urls:
-        # Ignore empty url
-        if not url:
-            continue
-        # Parse items from url
-        feed = feedparser.parse(url)
-        items = feed["items"]
-
-        # Remove published_parsed key which doesn't follow the JSON format
-        for item in items:
-            item.pop("published_parsed", None)
-
-        print(items)
+    items = get_items(urls)
+    print(items)
 
 
 if __name__ == "__main__":
