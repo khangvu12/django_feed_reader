f67d5c526992658711ff36ae79408fbe7af0c77e
khangvu12 <khang.vu@f4.intek.edu.vn>
2020-08-29 20:26:00 +0700

    add pagination

--- feed_reader/feed_reader_app/views.py
+++ feed_reader/feed_reader_app/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Item
 
-
 class ItemListView(ListView):
     model = Item
     context_object_name = 'item_list'   # Name for the list as a template variable
     template_name = 'feed_reader_app/item_list_template.html'  # Specify the template name/location
+    paginate_by = 5
 
-    def get_queryset(self):
-        return Item.objects.all() # Get items
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("paginate_by", self.paginate_by)
